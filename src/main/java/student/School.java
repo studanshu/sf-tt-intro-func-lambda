fceffbebd579214f4947ecafb5e07afbfc67b89c
Sudhanshu Bahety <sudhanshu.bahety@salesforce.com>
2019-01-08 12:28:50 -0800

    Adding functional interface

--- src/main/java/student/School.java
+++ src/main/java/student/School.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+@FunctionalInterface
 interface StudentCriterion {
 //    boolean broken();
     boolean test(Student s);
